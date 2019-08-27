421a1ff3fb34d744090f05afcd5bd08c7a497a85
Anurag Gokate <Anurag.Gokate@infostretch.com>
2019-08-27 18:10:52 +0530

    Committing

--- MySeleniumSessions/src/test/java/TestCar.java
+++
@@ -1,9 +0,0 @@
-
-public class TestCar {
-	public static void main(String[] args) {
-		BMW b = new BMW();
-		b.start();
-		b.stop();
-		Car.start(10);
-	}
-}
--- NewProject.java
+++
@@ -1,6 +0,0 @@
-
-public class NewProject {
-	public static void main(String[] args) {
-		System.out.println("Newwwwwwwwww");
-	}
-}
